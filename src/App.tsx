568b03b110dd8d8f6205645cd352a6e3d1fd0578
devahmedfarooq <ahmedfarooq.dev@gmail.com>
2024-12-09 06:13:00 -0800

    ..

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import ErrorPage from "./pages/Error/Error.page.tsx";
 import HomePage from "./pages/Home.page.tsx"
 import Success from "./pages/Payment/Success/Success.page.tsx";
 //import LoadPage from "./pages/Loading/Loading.page.tsx"
@@ -10,15 +11,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 function App() {
   // const { loading,/*  preview, seo, report, recomend */ } = useData()
   return (
-    
+
 
     <Router >
       <Routes>
         <Route path="/report/:id" element={<ReportPage />} />
         <Route path="/payment/success/:id" element={<Success />} />
-
         <Route path="/"
           element={<HomePage />} />
+        <Route path="*" element={<ErrorPage />} />
       </Routes>
     </Router>
 
